04df1a5f0a94713464238fa061282af7569a3402
Matheo Travers <matheo.daemon@gmail.com>
2024-12-03 13:21:23 +0100

    Add: day03 part 2

--- day03/day03.rs
+++ day03/day03.rs
@@ -11,6 +11,8 @@ fn mul(x:isize, y:isize) -> isize {
 fn search_line_mul(line: &str) -> Vec<isize> {
     let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
 
+    println!("{line}");
+
     let ok: Vec<isize> = re.find_iter(line).map(|m| {
         let origial_str: &str = m.as_str();
         let mut new_str: String = String::new();
@@ -28,9 +30,28 @@ fn search_line_mul(line: &str) -> Vec<isize> {
     ok
 }
 
+fn search_line_mul_secure(line: &str) -> Vec<Vec<isize>> {
+    let mut line_copy  = line.to_string().replace("\n", "");
+    line_copy.insert_str(0, "do()");
+    line_copy.insert_str(line_copy.len()-1, "don't()");
+
+    let re = Regex::new(r"do\(\)(.*?)don't\(\)").unwrap();
+
+    let ok: Vec<Vec<isize>> = re.find_iter(line_copy.as_str()).map(|m| {
+        search_line_mul(m.as_str())
+    }).collect();
+
+    ok
+}
+
 fn read_mul(file_path: &Path) -> Vec<Vec<isize>> {
     let contents = fs::read_to_string(file_path).expect("File not exist");
     return contents.lines().map(|s| search_line_mul(s)).collect::<Vec<_>>();
+}
+
+fn read_mul_secure(file_path: &Path) -> Vec<Vec<isize>> {
+    let contents = fs::read_to_string(file_path).expect("File not exist");
+    return search_line_mul_secure(contents.as_str())
 }
 
 fn main() {
@@ -38,12 +59,23 @@ fn main() {
     let file_path = Path::new("day03/input.txt");
     println!("In file {}", file_path.display());
     let vec_muls = read_mul(file_path);
-    let mut some_mul: isize = 0;
+    let vec_muls_secures = read_mul_secure(file_path);
+    let mut some_mul: isize = 0; 
+    let mut some_mul_secure: isize = 0;
+
+    // simple mutiplicateurs
     for vec_mul in vec_muls {
         for mul in vec_mul {
             some_mul += mul; 
         }
     }
 
-    println!("{some_mul}");
+    for vec_mul in vec_muls_secures {
+        for mul in vec_mul  {
+            some_mul_secure += mul;            
+        }
+    }
+
+    println!("mul: {some_mul}");
+    println!("mul secure: {some_mul_secure}");
 }
